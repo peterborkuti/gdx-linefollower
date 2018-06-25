cf018940ade1b21090c1034dbfece1613bf2d355
Peter Borkuti <peter@aspire>
2018-06-25 21:32:38 +0200

    start moving and drawing

--- core/src/hu/bp/gdxlinefollower/GdxLinefollower.java
+++ core/src/hu/bp/gdxlinefollower/GdxLinefollower.java
@@ -2,50 +2,92 @@ package hu.bp.gdxlinefollower;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import hu.bp.linefollowerrobot.Car;
+import hu.bp.linefollowerrobot.CarStateChange;
+
+import java.awt.*;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class GdxLinefollower extends ApplicationAdapter {
 	ShapeRenderer shapeRenderer;
 	OrthographicCamera camera;
-	SpriteBatch batch;
-	Texture img;
-	
+
+	private Car car;
+	private int x, y, halfTrackWidth, wheelRadius;
+	private double angle;
+	private Timer timer;
+	private Random rnd = new Random();
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 		shapeRenderer = new ShapeRenderer();
 		camera = new OrthographicCamera(600, 400);
+		setCar(new Car(30, 5));
+
+	}
+
+	public void setCar(Car car) {
+		this.car = car;
+		halfTrackWidth = (int)car.trackWidth;
+		wheelRadius = (int)(car.wheelDiameter / 2.0);
+
+		x = 0;
+		y = 0;
+		angle = 0;
+	}
+
+	public void drawCar(CarStateChange carStateChange) {
+		x += carStateChange.x;
+		y += carStateChange.y;
+		angle += carStateChange.angle;
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		CarStateChange carStateChange = moveCar();
+		x += carStateChange.x;
+		y += carStateChange.y;
+		angle += carStateChange.angle;
+
+		Gdx.gl.glClearColor(0, 1, 0, 1);
+
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
 		camera.update();
+
 		shapeRenderer.setProjectionMatrix(camera.combined);
 
-		shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
-		shapeRenderer.setColor(1, 1, 0, 1);
-		shapeRenderer.line(10, 10, 100, 100);
-		shapeRenderer.rect(100, 100, 10, 10);
-		shapeRenderer.circle(20, 30, 100);
-		shapeRenderer.end();
+		drawCar();
+	}
 
-		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-		shapeRenderer.setColor(0, 1, 0, 1);
-		shapeRenderer.rect(100, 100, 40, 70);
-		shapeRenderer.circle(30, 30, 50);
+	private CarStateChange moveCar() {
+		return car.move(Math.abs(rnd.nextInt(10)), Math.abs(rnd.nextInt(10)), 0.1);
+	}
+
+	private void drawCar() {
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+		shapeRenderer.setColor(Color.BLUE);
+
+		shapeRenderer.line(x - halfTrackWidth, y, x + halfTrackWidth, y);
+		shapeRenderer.line(x - halfTrackWidth, y - wheelRadius, x - halfTrackWidth, y + wheelRadius);
+		shapeRenderer.line(x + halfTrackWidth, y - wheelRadius, x + halfTrackWidth, y + wheelRadius);
+
+		shapeRenderer.setColor(Color.RED);
+
+		shapeRenderer.line(x, y, x, y + 4 * wheelRadius);
 		shapeRenderer.end();
 	}
-	
+
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		shapeRenderer.dispose();
 	}
 }
